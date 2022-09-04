d736b69baf1bf65acf8caaf843d6b51d2752a868
marcinoo01 <mistel940@gmail.com>
2022-09-04 21:07:44 +0200

    plugin for test + fixed one column in change-log-02.xml

---
+++ src/test/java/com/mmistela/AirlineReservationSystemAppTest.java
@@ -0,0 +1,10 @@
+package com.mmistela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AirlineReservationSystemAppTest {
+    @Test
+    public void airlineReservationSystemStart(){}
+}
